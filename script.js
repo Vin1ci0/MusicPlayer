15d08bb9724f7b991d6aaa21e144dff4019dc2e0
Vinicio Oliveira <oliveiravinicios31@gmail.com>
2022-04-04 10:16:14 -0300

    Funções adicionadas para barra da música

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let musica = document.querySelector('audio');
 
+//Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
+musica.addEventListener('timeupdate', atualizarBarra)
+
+//Funções
 function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
@@ -14,4 +18,9 @@ function pausarMusica() {
     musica.pause();
     document.querySelector('.botao-play').style.display = 'block';
     document.querySelector('.botao-pause').style.display = 'none'
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
 }
